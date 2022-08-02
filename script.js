dd82a5a5c9853549b09dffb77df7a469237e2581
Amálie Jirotková <amy.jirotkova@gmail.com>
2022-08-02 09:29:56 +0200

    podbarvení prvního tlačítka po kliknutí

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("JavaScript funguje!");
+
+console.log(Math.floor(Math.random()*10));
+/*
+let mojeBarvaVlasu = prompt("Jaká je tvoje barva vlasů?");
+console.log(mojeBarvaVlasu);
+
+
+let rokNarozeni = prompt("V jakém roce si se narodil?")
+
+let stari = new Date().getFullYear() - rokNarozeni
+
+console.log("Je ti " + stari + " let.")
+
+*/
+let tlacitko = document.querySelector(".button");
+
+function pozdrav(){
+    console.log("Ahoj!")
+    tlacitko.classList.toggle("barvapozadi");
+}
+
+tlacitko.addEventListener("click", pozdrav);
+
